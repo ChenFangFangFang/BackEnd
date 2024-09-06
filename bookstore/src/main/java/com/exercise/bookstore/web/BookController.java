4c74477f3bf05b0c4b1661c1000e31b6e255e27b
ChenFangFangFang <sophiachan0430@gmail.com>
2024-09-06 17:47:46 +0300

    Chapter 3, exercise 2,Bookstore: Listpage

--- bookstore/src/main/java/com/exercise/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/exercise/bookstore/web/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.exercise.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
@@ -17,6 +19,13 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
 
         return "books";
+    }
+
+    @RequestMapping(value = "/booklist", method = RequestMethod.GET)
+    public String requestMethodName(Model model) {
+        model.addAttribute("books", repository.findAll());
+
+        return "booklist";
     }
 
 }
